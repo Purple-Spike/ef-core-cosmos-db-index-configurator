a4355bc559622cf9132b2706e15e98654ef73ac2
Al Rodriguez <ProgrammerAl@outlook.com>
2022-12-31 18:01:42 -0500

    Created new project for attribute to mark properties as indexable enabled

---
+++ src/EfCoreCosmosDbIndexConfigurationLib/EfCoreCosmosDbIndexConfigurationLib/IncludeIndexAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace PurpleSpikeProductions.EfCoreCosmosDbIndexConfigurator.ConfigurationLib;
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class IncludeIndexAttribute : Attribute
+{
+
+}
